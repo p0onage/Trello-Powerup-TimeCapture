a2aeba3b7ffb32e7003ec147c9ca8c3dd821b115
Joshua Duxbury <btbhq@hotmail.com>
2017-01-17 13:35:41 +0000

    Debugging issue number obtained by regex

--- js/template.js
+++ js/template.js
@@ -10,6 +10,8 @@ var GetYouTrackLinkIfAvailable = function(t){
     var lowercaseName = cardName.toLowerCase();
     var issueNumberRegex = /[A-Za-z\d]{1,4}-[0-9]{1,5}/;
     var issueNumberMatches = lowercaseName.match(issueNumberRegex);
+    console.log("found match  : " + issueNumberMatches)
+    console.log("Issue Number  : " + issueNumberMatches[0])
     if(!issueNumberMatches){
       return null;
     }
